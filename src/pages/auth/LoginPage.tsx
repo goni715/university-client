118a0fbb566012c936d3e2e795d85d753a17cc34
goni715 <halesgoni715149123@gmail.com>
2024-10-16 10:14:22 +0600

    login-form

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,9 +1,34 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../../redux/features/auth/authApi";
 
 
 const LoginPage = () => {
+    const { register, handleSubmit } = useForm({
+      defaultValues: {
+        id: "A-0001",
+        password: "123456",
+      },
+    });
+    const [login, {data, isLoading, error} ] = useLoginMutation();
+    console.log(data);
+
+    const onSubmit = (data) => {
+        login(data);
+    }
     return (
         <>
-            <h1> This is Login Page</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label htmlFor="id">ID:</label>
+                    <input type="text" id="id" {...register('id')}/>
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="text" id="password" {...register('password')}/>
+                </div>
+                <Button htmlType="submit">Login</Button>
+            </form>
         </>
     );
 };
