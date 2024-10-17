4fc05f82c2f888e58e6d54f4aa370c7cf0228dcb
goni715 <halesgoni715149123@gmail.com>
2024-10-17 18:22:31 +0600

    verifyToken utils

--- src/redux/features/api/baseApi.ts
+++ src/redux/features/api/baseApi.ts
@@ -1,10 +1,20 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { RootState } from '../../store/store';
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'http://localhost:5000/api/v1',
+  credentials: 'include',
+  prepareHeaders(headers, {getState}) {
+    const token = (getState() as RootState).auth.token;
+     // If we have a token set in state, let's assume that we should be passing it
+     if(token){
+      headers.set('authorization', `${token}`)
+     }
+  }
+});
 
 export const baseApi = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({
-     baseUrl: 'http://localhost:5000/api/v1',
-     credentials: 'include'
-  }),
+  baseQuery: baseQuery,
   endpoints: () => ({})
 })
