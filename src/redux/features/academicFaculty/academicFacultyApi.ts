5746dd676ccaa95649ea755592af30862d53e285
goni715 <halesgoni715149123@gmail.com>
2024-10-19 23:47:47 +0600

    academicFaculty ap

---
+++ src/redux/features/academicFaculty/academicFacultyApi.ts
@@ -0,0 +1,26 @@
+import { baseApi } from "../api/baseApi"
+
+
+const academicFacultyApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllAcademicFaculties: builder.query({
+      query: () =>{
+        return {
+          url: '/academic-faculty/get-all-academic-faculties',
+          method: 'GET',
+        }
+      },
+      providesTags: ['AcademicFaculty']
+    }),
+    addAcademicFaculty: builder.mutation({
+      query: (data) => ({
+        url: '/academic-faculty/create-academic-faculty',
+        method: 'POST',
+        body: data
+      }),
+      invalidatesTags: ['AcademicFaculty']
+    }),
+  }),
+})
+
+export const { useGetAllAcademicFacultiesQuery, useAddAcademicFacultyMutation } = academicFacultyApi;
