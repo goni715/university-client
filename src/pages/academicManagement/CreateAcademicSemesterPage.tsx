f7be393f5dfd31930479d87c5ef24440ea94b06d
goni715 <halesgoni715149123@gmail.com>
2024-10-19 09:30:01 +0600

    semester-form

--- src/pages/academicManagement/CreateAcademicSemesterPage.tsx
+++ src/pages/academicManagement/CreateAcademicSemesterPage.tsx
@@ -1,8 +1,19 @@
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import PHForn from "../../components/form/PHForn";
+import PHInput from "../../components/form/PHInput";
+import { Button } from "antd";
 
 const CreateAcademicSemesterPage = () => {
+
+    const onSubmit : SubmitHandler<FieldValues> = (data) => {
+        console.log(data);
+    }
     return (
         <>
-         <h1> CReate Academic Semester Page</h1>   
+         <PHForn onSubmit={onSubmit}>
+            <PHInput type="text" name="name" label="Name"/>
+            <Button htmlType="submit">Submit</Button>
+        </PHForn>  
         </>
     );
 };
